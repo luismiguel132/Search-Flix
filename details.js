92c6908f31f12baaafeba75e9a43e2603007ed21
luismiguel132 <132718966+luismiguel132@users.noreply.github.com>
2025-11-20 18:44:35 -0300

    ✨trailers

--- details.js
+++ details.js
@@ -10,6 +10,7 @@ const movieRating = document.getElementById('movie-rating');
 const movieImdb = document.getElementById('movie-imdb');
 const movieGeners = document.getElementById('movie-geners');
 const MovieCategory = document.querySelector('.movie-category');
+const MovieTrailer = document.getElementById('movie-trailer') 
 
 const urlParams = new URLSearchParams(window.location.search);
 const movieId = urlParams.get('id');
@@ -37,9 +38,6 @@ async function loadDetails() {
     );
 
     const filme = await response.json();
-
-    console.log('MOVIE', filme);
-    console.log('Movie IMAGE:', filme.poster_path);
 
     movieImage.src = `https://image.tmdb.org/t/p/w500${filme.backdrop_path}`;
     movieTitle.textContent = filme.title;
@@ -78,8 +76,39 @@ function updateCarousel(genre) {
   waitForCarousel();
 }
 
+async function loadTrailer() {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${API_KEY_TMDB}&language=${currentLanguage}`
+    );
+    const data = await response.json();
+    const videos = data.results;
+
+    console.log('Dados do FILME >>>', videos);
+
+    const trailer = videos[0].key
+
+    console.log('Trailer KEY >>>', trailer);
+
+    if(!trailer) {
+      return
+    }
+
+    MovieTrailer.innerHTML = `
+    <div class="m-8 rounded-lg overflow-hidden flex items-center justify-center">
+      <iframe width="860" class="w-full" height="415" src="https://www.youtube.com/embed/${trailer}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+      `
+    
+    } catch (error) {
+    console.warn('Trailer não encontrado');
+  }
+}
+
+
 if (movieId) {
   loadDetails();
+  loadTrailer();
 }
 
 
